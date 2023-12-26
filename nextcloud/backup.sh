14e49f4e21b926c248b21416d89964f74fee33fd
remi.germe <remi.germe@gmail.com>
2023-12-26 11:51:56 +0100

    maintenance mode

--- nextcloud/backup.sh
+++ nextcloud/backup.sh
@@ -1,9 +1,15 @@
+echo Entering maintenance mode
+sudo docker exec -u www-data nextcloud php /var/www/html/occ maintenance:mode --on
+
 sudo mount UUID=uuid_here_backup /mnt/backup
 echo Disk mounted
 
 echo Starting data folder backup
-sudo rsync -Aax /mnt/prod/data/ /mnt/backup/data/
+sudo rsync -Aaxu /mnt/prod/data/ /mnt/backup/data/
 echo Data folder backup done
 
 sudo umount /mnt/backup
 echo Disk unmounted, backup done
+
+sudo docker exec -u www-data nextcloud php /var/www/html/occ maintenance:mode --off
+echo Maintenance mode over
