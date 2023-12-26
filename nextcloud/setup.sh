15247d9b361b9d70f7cca5799e85358df40f619e
remi.germe <remi.germe@gmail.com>
2023-12-26 16:57:39 +0100

    backup disk in /etc/fstab

--- nextcloud/setup.sh
+++ nextcloud/setup.sh
@@ -5,11 +5,18 @@ echo Nextcloud setup : starting
 read -p "UUID prod disk : " uuid_prod
 if ! grep -q "$uuid_prod" /etc/fstab; then
     echo UUID=$uuid_prod /mnt/prod ext4 defaults 0 0 >> /etc/fstab
-    echo Added disk to /etc/fstab
+    echo Added prod disk to /etc/fstab
 else
-    echo Disk already in /etc/fstab, check config
+    echo Prod disk already in /etc/fstab, check config
 fi
 
+read -p "UUID backup disk : " uuid_backup
+if ! grep -q "$uuid_backup" /etc/fstab; then
+    echo UUID=$uuid_backup /mnt/backup ext4 defaults 0 0 >> /etc/fstab
+    echo Added backup disk to /etc/fstab
+else
+    echo Backup disk already in /etc/fstab, check config
+fi
 
 [ ! -f nextcloud/disk.env ] && touch nextcloud/disk.env
 if ! grep -q "UUID_PROD=" nextcloud/disk.env; then
@@ -18,29 +25,28 @@ if ! grep -q "UUID_PROD=" nextcloud/disk.env; then
 else
     echo Prod disk already provided in nextcloud/disk.env, check config
 fi
-
-mkdir -p /mnt/prod
-mount -a
-mkdir -p /mnt/prod/data
-chown -R www-data:sudo /mnt/prod/data
-chmod 750 /mnt/prod/data
-
-mkdir -p /mnt/backup
-read -p "UUID backup disk : " uuid_backup
-mount UUID=$uuid_backup /mnt/backup
-mkdir -p /mnt/backup/data
-chown -R www-data:sudo /mnt/backup/data
-chmod 770 /mnt/backup/data
-mkdir -p /mnt/backup/mysql
-chown -R www-data:sudo /mnt/backup/mysql
-chmod 770 /mnt/backup/mysql
-
 if ! grep -q "UUID_BACKUP=" nextcloud/disk.env; then
     echo UUID_BACKUP=$uuid_backup >> nextcloud/disk.env
     echo Added backup disk to nextcloud/disk.env
 else
     echo Backup disk already provided in nextcloud/disk.env, check config
 fi
+
+mkdir -p /mnt/prod
+mkdir -p /mnt/backup
+
+mount -a
+
+mkdir -p /mnt/prod/data
+chown -R www-data:sudo /mnt/prod/data
+chmod 750 /mnt/prod/data
+
+mkdir -p /mnt/backup/data
+chown -R www-data:sudo /mnt/backup/data
+chmod 770 /mnt/backup/data
+mkdir -p /mnt/backup/mysql
+chown -R www-data:sudo /mnt/backup/mysql
+chmod 770 /mnt/backup/mysql
 
 echo Configuring backup crontab : starting
 chmod +x nextcloud/backup.sh
