a8e53af65240c0519b45ce6966506a1aeb942c85
remi.germe <remi.germe@gmail.com>
2023-12-26 14:39:17 +0100

    backup mysql

--- nextcloud/backup.sh
+++ nextcloud/backup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-echo Entering maintenance mode
-sudo docker exec -u www-data nextcloud php /var/www/html/occ maintenance:mode --on
+export $(xargs < .env)
 
-sudo mount UUID=uuid_here_backup /mnt/backup
+echo Entering maintenance mode
+docker exec -u www-data nextcloud php /var/www/html/occ maintenance:mode --on
+
+mount UUID=$UUID_BACKUP /mnt/backup
 echo Disk mounted
 
-echo Starting data folder backup
-sudo rsync -Aaxu /mnt/prod/data/ /mnt/backup/data/
-echo Data folder backup done
+echo Data folder backup : starting
+rsync -Aaxu /mnt/prod/data/ /mnt/backup/data/
+echo Data folder backup : done
 
-sudo umount /mnt/backup
+echo Dumping db : starting
+docker exec nextcloud-mariadb sh -c '(mysqldump --single-transaction -h mariadb -u "'"$MYSQL_USER"'" -p"'"$MYSQL_PASSWORD"'" "'"$MYSQL_DB"'" > /backup/nextcloud_`date +"%Y%m%d"`.bak)'
+echo Dumping db : done
+
+umount /mnt/backup
 echo Disk unmounted, backup done
 
-sudo docker exec -u www-data nextcloud php /var/www/html/occ maintenance:mode --off
+docker exec -u www-data nextcloud php /var/www/html/occ maintenance:mode --off
 echo Maintenance mode over
