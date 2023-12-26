a5028ead7d92d6c553cb4d335d8b401d7d2993a6
remi.germe <remi.germe@gmail.com>
2023-12-26 18:55:40 +0100

    logging formatting

--- nextcloud/backup.sh
+++ nextcloud/backup.sh
@@ -2,16 +2,20 @@
 
 export $(xargs < /home/raimmy/remigerme.xyz/nextcloud/.env)
 
-echo Entering maintenance mode
+mylog () {
+    echo "[$(date +"%Y-%m-%d %H:%M:%S")] $1"
+}
+
+mylog "Entering maintenance mode"
 docker exec -u www-data nextcloud php /var/www/html/occ maintenance:mode --on
 
-echo Data folder backup : starting
+mylog "Data folder backup : starting"
 rsync -Aau /mnt/prod/data/ /mnt/backup/data/
-echo Data folder backup : done
+mylog "Data folder backup : done"
 
-echo Dumping db : starting
+mylog "Dumping db : starting"
 docker exec nextcloud-mariadb sh -c '(mysqldump --single-transaction -h mariadb -u "'"$MYSQL_USER"'" -p"'"$MYSQL_PASSWORD"'" "'"$MYSQL_DB"'" > /backup/nextcloud_`date +"%Y%m%d"`.bak)'
-echo Dumping db : done
+mylog "Dumping db : done"
 
 docker exec -u www-data nextcloud php /var/www/html/occ maintenance:mode --off
-echo Maintenance mode over
+mylog "Maintenance mode over"
--- nextcloud/disk.sh
+++ nextcloud/disk.sh
@@ -2,20 +2,24 @@
 
 export $(xargs < /home/raimmy/remigerme.xyz/nextcloud/disk.env)
 
-echo Checking prod disk
+mylog () {
+    echo "[$(date +"%Y-%m-%d %H:%M:%S")] $1"
+}
+
+mylog "Checking prod disk"
 if ! lsblk | grep -q "/mnt/prod"; then
-    echo Prod disk not found, shutting down
+    mylog "Prod disk not found, shutting down"
     docker compose -f /home/raimmy/remigerme.xyz/nextcloud/docker-compose.nextcloud.yml down
     umount /mnt/backup
     exit 1
 fi
-echo Prod disk fine
+mylog "Prod disk fine"
 
-echo Checking backup disk
+mylog "Checking backup disk"
 if ! lsblk | grep -q "/mnt/backup"; then
-    echo Backpup disk not found, shutting down
+    mylog "Backpup disk not found, shutting down"
     docker compose -f /home/raimmy/remigerme.xyz/nextcloud/docker-compose.nextcloud.yml down
     umount /mnt/prod
     exit 1
 fi
-echo Backup disk fine
+mylog "Backup disk fine"
