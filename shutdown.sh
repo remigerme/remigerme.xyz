43788c76d9ccb77b1796fd25d5a84b0ec1326d74
remi.germe <remi.germe@gmail.com>
2024-01-06 15:01:00 +0100

    shutdown shortcut

---
+++ shutdown.sh
@@ -0,0 +1,4 @@
+docker compose -f docker-compose.traefik.yml down
+docker compose -f home/docker-compose.home.yml down
+docker compose -f yourls/docker-compose.yourls.yml down
+docker compose -f nextcloud/docker-compose.nextcloud.yml down
