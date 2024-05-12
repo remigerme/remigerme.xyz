5dec5fdc16196de829382f3b46834886093067d9
remi.germe <remi.germe@gmail.com>
2024-05-12 12:37:40 +0200

    CIDR range IP reverse proxy

--- nextcloud/additionnal_setup.sh
+++ nextcloud/additionnal_setup.sh
@@ -3,7 +3,7 @@
 echo Starting nextcloud instance
 docker compose -f /home/raimmy/remigerme.xyz/nextcloud/docker-compose.nextcloud.yml up -d --build
 docker exec -u www-data -it nextcloud php occ config:system:set overwriteprotocol --value='https'
-docker exec -u www-data -it nextcloud php occ config:system:set trusted_proxies 0 --value=172.0.xxx.xxx
+docker exec -u www-data -it nextcloud php occ config:system:set trusted_proxies 0 --value=172.0.0.0/16
 docker exec -u www-data -it nextcloud php occ config:system:set default_phone_region --value='FR'
 docker exec -u www-data -it nextcloud php occ config:system:set maintenance_window_start --type=integer --value=1
 docker compose -f /home/raimmy/remigerme.xyz/nextcloud/docker-compose.nextcloud.yml down
