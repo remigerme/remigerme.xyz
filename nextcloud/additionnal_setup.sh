6d96d2d5e2833483a12f91f81c54af16716d0e10
remi.germe <remi.germe@gmail.com>
2024-05-12 12:23:41 +0200

    maintenance window

--- nextcloud/additionnal_setup.sh
+++ nextcloud/additionnal_setup.sh
@@ -5,5 +5,6 @@ docker compose -f /home/raimmy/remigerme.xyz/nextcloud/docker-compose.nextcloud.
 docker exec -u www-data -it nextcloud php occ config:system:set overwriteprotocol --value='https'
 docker exec -u www-data -it nextcloud php occ config:system:set trusted_proxies 0 --value=172.0.xxx.xxx
 docker exec -u www-data -it nextcloud php occ config:system:set default_phone_region --value='FR'
+docker exec -u www-data -it nextcloud php occ config:system:set maintenance_window_start --type=integer --value=1
 docker compose -f /home/raimmy/remigerme.xyz/nextcloud/docker-compose.nextcloud.yml down
 echo Shutting down nextcloud instance
