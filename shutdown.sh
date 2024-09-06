8f71e5f6d90b32bc926837b704f5121faaf50ed8
remi.germe <remi.germe@gmail.com>
2024-09-06 23:14:21 +0200

    start and stop quartz

--- shutdown.sh
+++ shutdown.sh
@@ -1,4 +1,5 @@
-docker compose -f docker-compose.traefik.yml down
-docker compose -f home/docker-compose.home.yml down
-docker compose -f yourls/docker-compose.yourls.yml down
 docker compose -f nextcloud/docker-compose.nextcloud.yml down
+docker compose -f my-quartz/docker-compose.yml down
+docker compose -f yourls/docker-compose.yourls.yml down
+docker compose -f home/docker-compose.home.yml down
+docker compose -f docker-compose.traefik.yml down
